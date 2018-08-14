15717a4429016814befdc58db7f0a6126ea0837f
prasad ghole <prasad.ghole@gmail.com>
2018-08-14 08:59:48 +0000

    Uniform init added

---
+++ UniformInit.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+class MyClass {
+    private:
+        int m_var ;
+        
+    public :
+        MyClass() = default;
+        MyClass(const MyClass& rhs) = default;
+        void Echo(void)
+        {
+            cout << "I am Myclass" << endl;
+        }        
+};
+
+int main()
+{
+    MyClass A;
+    
+    //MyClass B(MyClass());
+    MyClass B{MyClass()};
+    
+    B.Echo();
+    return 0;    
+}
